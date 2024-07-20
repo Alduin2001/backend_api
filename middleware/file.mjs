4a1c39012f427b049f27c320aa58d6d58dc378b5
Alduin <almas20_01@mail.ru>
2024-07-20 13:06:35 +0300

    Second commit

---
+++ middleware/file.mjs
@@ -0,0 +1,38 @@
+import multer, { diskStorage } from "multer";
+const allowedTypes = ['image/png','image/jpeg'];
+import path from 'path';
+import fs from 'fs';
+
+const storage = diskStorage({
+    destination:function(req,file,cb){
+        cb(null,path.join(__dirname,'../uploads'));
+    },
+    filename:function(req,file,cb){
+        cb(null,`${Date.now}-${file.originalname}`);
+    }
+})
+
+const upload = multer({
+    storage,
+    fileFilter:function(req,file,cb){
+        if(allowedTypes.includes(file.mimetype)){
+            cb(null,true);
+        }else{
+            cb(new Error('Не поддерживаемый тип файла'),false);
+        }
+    },
+    limits:{
+        fileSize:1024*1024*10
+    }
+});
+
+const deleteFileMiddleware = (file)=>{
+    try{
+        fs.unlink(path.join(__dirname,'../uploads',file));
+        next();
+    }catch(err){
+        console.log(err);
+    }
+}
+
+export {upload,deleteFileMiddleware};
